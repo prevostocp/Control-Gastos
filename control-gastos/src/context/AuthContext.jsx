791902ebcaa93719fa96de117d215b1551801f40
DavidPrevosto <dprevosto@cordobasoftware.com.ar>
2025-06-10 17:20:19 -0300

    curso 10062025

---
+++ control-gastos/src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import { supabase } from "../index"
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        const { data: authListener } = supabase.auth.onAuthStateChange(
+            async (session) => {
+                if (session == null) {
+                    setUser(null);
+                } else {
+                    console.log(session);
+                }
+            }
+        )
+        return () => {
+            authListener.subscription
+        }
+    }, []);
+    return (
+        <AuthContext.Provider value={{user}} >{children}</AuthContext.Provider>
+    );
+}
+export const UserAuth = () => {
+    return useContext(AuthContext)
+}
