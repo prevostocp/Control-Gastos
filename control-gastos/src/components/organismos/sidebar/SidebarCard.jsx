0a49c3a713bf3bfd9d04508703d1be6c92b3a181
David Prevosto <david_zeppelin@hotmail.com>
2025-06-18 21:41:03 -0300

    curso dia 18062025

--- control-gastos/src/components/organismos/sidebar/SidebarCard.jsx
+++ control-gastos/src/components/organismos/sidebar/SidebarCard.jsx
@@ -4,8 +4,20 @@ import { v } from '../../../index'
 export function SidebarCard() {
   return (<Container>
     <span className="icon" >{<v.iconoayuda />}</span>
+    <div className="carContent">
+      <div className="circle1"></div>
+      <div className="circle2" ></div>
+
+      <h3>Centro de ayuda</h3>
+
+      <button className="btn" >Conectar</button>
+    </div>
   </Container>);
 }
-const Container =styled.div`
-  
+const Container = styled.div`
+  width: 100;
+  padding: 1rem;
+  text-align: center;
+  position: relative;
+
 `
